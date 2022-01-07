75b8172bd6b8aa9272cb4cd30beb279d369ab7dd
Mauricio Paternina <spaceinvadev@icloud.com>
2022-01-07 12:33:47 -0500

    set up deployment npm script

--- server/src/app.js
+++ server/src/app.js
@@ -1,24 +1,36 @@
 const express = require('express')
+const path = require('path')
+
 const cors = require('cors')
 
 const planetsRouter = require('./routes/planets/planets.router')
 
 const app = express()
 
-const corsWhitelist = ['http://localhost:3000', 'FINAL_URL']
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (corsWhitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS!'))
-    }
-  }
-}
+// const corsWhitelist = ['http://localhost:3000', 'http://localhost:8000']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (corsWhitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS!'))
+//     }
+//   }
+// }
 
 // Middlewares
-app.use(cors(corsOptions))
+app.use(
+  cors({
+    origin: 'http://localhost:3000'
+  })
+)
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
 app.use(planetsRouter)
+
+app.get('/', (_, res) =>
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+)
 
 module.exports = app
