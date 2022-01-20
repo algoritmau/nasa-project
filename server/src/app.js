9a9a14ce4c76b12ac779c0e6f34c4c8971c6aad3
Mauricio Paternina <spaceinvadev@icloud.com>
2022-01-20 17:24:47 -0500

    add morgan to improve logging

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 
 const cors = require('cors')
+const morgan = require('morgan')
 
 const planetsRouter = require('./routes/planets/planets.router')
 
@@ -24,6 +25,8 @@ app.use(
     origin: 'http://localhost:3000'
   })
 )
+app.use(morgan('combined'))
+
 app.use(express.json())
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
